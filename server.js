514d9d060ca55ac247ae7bfe8ffd2549ea1b3d7d
World05 <worldgoggo05@gmail.com>
2024-11-22 05:54:21 +0900

    MongoDB Connection

--- server.js
+++ server.js
@@ -1,57 +1,23 @@
-console. log ("Web Serverni boshlash");
-const express = require ("express");
-const app = express();
 const http = require ("http");
-const fs = require("fs");
+const mongodb = require ("mongodb");
 
-let user;
-fs.readFile("database/user.json", "utf-8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    } else {
-        user = JSON.parse(data);
+let db;
+const connectionString = "mongodb+srv://worldgoggo05:98x65zLMxhDgWu6q@cluster0.hxb9o.mongodb.net/Reja?retryWrites=true&w=majority&appName=Cluster0";
+
+mongodb.connect(connectionString, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}, (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB");
+    else {
+        console.log("MongoDB connection succeed");
+        module.exports = client;
+        const app = require("./app");
+        const server = http.createServer(app);
+        let PORT = 3000;
+        server.listen(PORT, function() {
+            console.log(
+                `Server is running on port ${PORT}, http://localhost:${PORT}`);
+});
     }
-})
-
-// 1 - Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// 2 Session code
-
-// 3 - Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 - Routing code
-// app.get("/hello", function (req, res) {
-//     res.end("<h1 style='color: red;'> HELLO WORLD</h1>");
-// });
-
-// app.get("/gift", function (req, res) {
-//     res.end("<h1 style='background-color: red;'> Siz sovg'alar sahifasidasiz </h1>");
-// });
-
-app.get("/author", (req, res) => {
-    res.render("author", {user: user});
-});
-app.get("/author1", (req, res) => {
-    res.render("author1", {user: user});
-});
-
-
-app.post ("/create-item", (req, res) => {
-    console.log(req);
-    res.json({ test: "success "});
-});
-
-app.get("/", function (req, res) {
-    res.render("reja");
-});
-
-
-const server = http.createServer(app);
-let PORT = 3000;
-server .listen(PORT, function () {
-    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
 });
