abcd24d7700248981ca91db7e0b3e6a249d9f544
World05 <worldgoggo05@gmail.com>
2024-12-01 21:17:02 +0900

    Port => 1904

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ mongodb.connect(connectionString, {
         module.exports = client;
         const app = require("./app");
         const server = http.createServer(app);
-        let PORT = process.env.PORT || 3000;
+        let PORT = process.env.PORT || 1904;
         server.listen(PORT, function() {
             console.log(
                 `Server is running on port ${PORT}`);
