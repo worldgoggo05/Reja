063dd3fcc4a399dffad166dff9bc9bb2099d90e5
World05 <worldgoggo05@gmail.com>
2024-12-04 03:35:33 +0900

    F-Task Done

--- train.js
+++ train.js
@@ -1,6 +1,15 @@
-// E-Task
-function rvrs (str){
-    return str.toLowerCase().split('').reverse().join('')
-}
-console.log(rvrs('hello'))  //olleh
+//F-Task
 
+function dbChars(str) {
+    // str.length-1 ensures we don't go out of bounds when checking next character
+    for (let i = 0; i < str.length - 1; i++) {
+        // str[i]     - current character
+        // str[i+1]   - next character
+        // Check if current and next characters are identical
+        if (str[i] == str[i + 1]) {
+            return true   
+        }
+    }
+    return false    
+}
+console.log(dbChars('hello')) //true 
